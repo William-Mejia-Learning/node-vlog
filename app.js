5b70d20fb8446f718a82d6aebfed1c5166fcb65f
williamxmejia <willx1995@gmail.com>
2022-09-29 15:07:24 -0500

    feat: mysql data dyanmically displays on ejs page

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const cookieParser =require('cookie-parser');
 const bodyParser = require('body-parser');
 const db = require('./database');
 const app = express();
+const ejs = require('ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/views')));
 app.use(
   session({
     secret: '123@123abc',
@@ -41,6 +42,23 @@ app.post('/user_form', function (req, res, next) {
     res.redirect('/');
   })
 });
+
+app.get('/users', (req,res) => {
+  db.query(`SELECT * FROM users`, function(err, result) {
+    if(err) throw err;
+    res.render('display', {data:result});
+  });
+});
+
+app.get('/articles', (req, res) => {
+  let articles = [
+    {title: "Man Discovers Different Opinion", author: "Reginald", date: "9/2/45"},
+    {title: "Tigers Aren't Great Pets", author: "Simon", date: "4/13/95"},
+    {title: "Eating Cake for Breakfast", author: "Katie", date: "8/20/13"}
+  ];
+
+  res.render("articles.ejs", {articles: articles});
+})
 
 app.use(function (req, res, next) {
   next(createError(404));
